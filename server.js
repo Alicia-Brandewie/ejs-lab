c77c4c1fb0c0d22514456b29a823d89a87533f3d
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-16 21:50:09 -0500

    the backend is beauuuutiful but the frontend is not

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 
-  const RESTAURANT = {
+const RESTAURANT = {
   name: 'The Green Byte Bistro',
   isOpen: true,
   address: '742 Evergreen Rd, Mapleview, OS 45502',
   phone: '555-321-9876',
   menu: [
-    { 
+    {
       id: 1,
       name: 'Quantum Quinoa Mushroom Burger',
       price: 13.00,
@@ -16,7 +16,7 @@ const app = express();
       category: 'mains',
       details: 'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
     },
-    { 
+    {
       id: 2,
       name: 'Binary Berry Cheesecake',
       price: 10.11,
@@ -24,7 +24,7 @@ const app = express();
       category: 'desserts',
       details: 'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
     },
-    { 
+    {
       id: 3,
       name: 'Recursive Rigatoni',
       price: 17.00,
@@ -32,7 +32,7 @@ const app = express();
       category: 'mains',
       details: 'A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You\'ll keep coming back for more.'
     },
-    { 
+    {
       id: 4,
       name: 'Pumpkin Pi Squared',
       price: 3.14,
@@ -40,7 +40,7 @@ const app = express();
       category: 'desserts',
       details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
     },
-    { 
+    {
       id: 5,
       name: 'Fibonacci String Bean Fries',
       price: 11.23,
@@ -64,8 +64,21 @@ app.get('/', (req, res) => {
 
 app.get('/menu', (req, res) => {
   res.render('menu.ejs', {
-   restaurant: RESTAURANT.menu
+    restaurant: RESTAURANT.menu
   });
 });
+
+app.get('/menu/:category', (req, res) => {
+  res.render('category.ejs', {
+    category: req.params.category,
+    menuItems: RESTAURANT.menu.filter((banana) => {
+      if (banana.category === req.params.category) {
+        return banana
+      }
+    }
+    )
+  }
+  )
+})
 
 app.listen(3000);
